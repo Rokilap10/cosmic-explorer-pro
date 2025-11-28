386ece1921790d586060350af41e88c768d12673
akerkeww <akerkeshaaa13@gmail.com>
2025-11-28 16:58:34 +0500

    feat: add basic ContentEditor class with initialization

---
+++ admin.js
@@ -0,0 +1,10 @@
+class ContentEditor {
+    constructor() {
+        this.isEditing = false;
+        this.init();
+    }
+
+    init() {
+        console.log('Content Editor initialized');
+    }
+}
